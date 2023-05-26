d89a6fe314ca3f94fd786345b6a17f0be6db26bf
Vantom231 <szonek230@gmail.com>
2023-05-26 17:47:29 +0200

    User api index, show and resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'firstName' => $this->first_name,
+            'lastName' => $this->last_name,
+            'pesel' => $this->pesel,
+            'email' => $this->email,
+            'emailVerifiedAt' => $this->email_verified_at,
+            'mailingAddress' => $this->mailing_address,
+            'companyName' => $this->company_name,
+            'nip' => $this->nip,
+            'companyAddress' => $this->company_address,
+            'companyMailingAddress' => $this->company_mailing_address,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+            'accountType' => $this->account_type,
+            ];
+    }
+}
