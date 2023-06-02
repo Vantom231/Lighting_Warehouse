1523a93868ea958a4decbe8d2f32420fca90f0bd
Vantom231 <szonek230@gmail.com>
2023-06-02 23:54:53 +0200

    basic register and login api

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,12 @@ use App\Http\Controllers\OrderToProductController;
 use App\Http\Controllers\OrderToUserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SubcategoryController;
+use App\Http\Requests\AuthRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,7 +29,7 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::group(['namespace' => 'App\Http\Controllers'], function() {
+Route::group(['namespace' => 'App\Http\Controllers', 'middleware' => 'auth:sanctum'], function() {
     Route::apiResource('users', UserController::class);
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('subcategories', SubcategoryController::class);
@@ -32,4 +37,21 @@ Route::group(['namespace' => 'App\Http\Controllers'], function() {
     Route::apiResource('products', ProductController::class);
     Route::apiResource('orderToProducts', OrderToProductController::class);
     Route::apiResource('orderToUsers', OrderToUserController::class);
+});
+
+
+Route::get('login', function (AuthRequest $request) {
+    if (Auth::attempt($request->all())){
+
+        $user = Auth::user();
+        if (count(Auth::user()->tokens) != 0) {
+            $user->tokens()->delete();
+        }
+        return ['token' => $user->createToken('exampleToken')->plainTextToken];
+
+    }
+});
+
+Route::post('register', function (StoreUserRequest $request) {
+    return new UserResource(User::create($request->all()));
 });
